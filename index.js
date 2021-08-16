ed9484981b5650f4b1fd718f757f9bad259275ba
Thomasalex2 <Thomas321alex@gmail.com>
2021-08-16 19:53:27 +0000

    feat: Added two more questions style: Added chalk pkg style: reformatted presentation of quiz

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var readlineSync = require("readline-sync")
+const chalk = require('chalk')
 var points = 0
 
 var questions = [ 
   QuestionOne = { 
-    question: "What is my name? ",
+    question: "What is Thomas's fullname? ",
     answer: "Thomas Alex"},
   QuestionTwo = {
     question: "Where is he from? ",
@@ -11,30 +12,37 @@ var questions = [
   QuestionThree = {
     question: "Where did he study for college? ",
     answer: "Manipal Institute of Technology"},
+  QuestionFour = {
+    question: "Where did he live most of his life? ",
+    answer: "Dubai"},
+  QuestionFive = {
+    question: "Did he colour his hair blue at one point? ",
+    answer: "Yes"},
   ]
 
 function playQuiz (question, answer) {
-  var userAnswer = readlineSync.question(question)
+  var userAnswer = readlineSync.question(chalk.red(question))
 
   if (userAnswer.toLowerCase() === answer.toLowerCase()) {
-    console.log ("Correct Answer! :)")
+    console.log ("Correct Answer! :)\n")
     points++
   } else {
-    console.log("Wrong Answer :(")
+    console.log("Wrong Answer :(\n")
   }
 }
 
-var userName = readlineSync.question("What is your name? ")
-console.log("Welcome " + userName)
-console.log("This is quiz about Thomas Alex \n")
+var userName = readlineSync.question(chalk.yellow("What is your name? "))
+console.log(chalk.green("\nWelcome " + userName))
+console.log(chalk.green("This is quiz about Thomas Alex \nAll the best! \n"))
 
-for (var i = 0; i < 3; i++) {
+var size = questions.length
+for (var i = 0; i < size; i++) {
   playQuiz(questions[i].question, questions[i].answer)
 }
 
 if (points >= 2) {
-  console.log("Congratulations " + userName +"!")
+  console.log(chalk.cyan("Congratulations " + userName +"!"))
 } else {
-  console.log("Better luck next time!")
+  console.log(chalk.cyan("Better luck next time!"))
 }
-console.log ("Your final score is: ", points)
+console.log (chalk.cyan("Your final score is: ", points))
